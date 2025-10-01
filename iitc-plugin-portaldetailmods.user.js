901b74ece637be5d495210f88930b60b90f1ea7b
Richard Poser <39813882+Whomiga@users.noreply.github.com>
2025-10-01 08:31:02 -0500

    Added meta file and updated URLs

--- iitc-plugin-portaldetailmods.user.js
+++ iitc-plugin-portaldetailmods.user.js
@@ -6,6 +6,7 @@
 // @version        0.0.1
 // @description    Show Mod Pictures in Portal Details
 // @downloadURL    https://raw.githubusercontent.com/Whomiga/iitc-plugin-portaldetailsmods/refs/heads/master/iitc-plugin-portaldetailmods.user.js
+// @updateURL      https://raw.githubusercontent.com/Whomiga/iitc-plugin-portaldetailsmods/refs/heads/master/iitc-plugin-portaldetailmods.meta.js
 // @match          *://intel.ingress.com/*
 // @grant          none
 // ==/UserScript==
