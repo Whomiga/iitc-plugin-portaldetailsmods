d307a0a85c00d59a2d9c3f44b399ccff2235d287
Richard Poser <39813882+Whomiga@users.noreply.github.com>
2026-01-18 12:04:17 -0600

    - Converted all the graphics from PNG to WEBP, lots of space saved

--- iitc-plugin-portaldetailmods.meta.js
+++ iitc-plugin-portaldetailmods.meta.js
@@ -3,7 +3,7 @@
 // @id             portaldetailmods@Whomiga
 // @name           Portal Detail Mods
 // @category       Info
-// @version        0.1.0
+// @version        0.2.0
 // @description    Show Mod Pictures in Portal Details
 // @downloadURL    https://www.missingpiece.com/ingress/IITC/iitc-plugin-portaldetailmods.user.js
 // @updateURL      https://www.missingpiece.com/ingress/IITC/iitc-plugin-portaldetailmods.meta.js
