af0ed3b2ec6020d5451166810edd90ff1406d161
Richard Poser <39813882+Whomiga@users.noreply.github.com>
2026-07-27 12:24:44 -0500

    - Fixed slight modification for OK Button handling

--- iitc-plugin-portaldetailmods.user.js
+++ iitc-plugin-portaldetailmods.user.js
@@ -779,7 +779,7 @@ function wrapper(plugin_info) {
             localStorage_Save();
         }
         else {
-            dialog_handleOkButton();
+            dialog_handleOKButton();
         }
     }
 
